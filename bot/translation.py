ef5802b20c88cc6e5d9158bf343975cb39f55f2a
KaneKenaz <89187476+KaneKenaz@users.noreply.github.com>
2021-08-19 14:34:09 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -4,13 +4,13 @@
 
 class Translation(object):
     
-    START_TEXT = """<b>Hey {}!!</b>
+    START_TEXT = """<b><u>Hey {}!!</b></u>
 <i>Am Just A Advance Auto Filter Bot....😉
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+ഇവിടെ നോക്കി നിന്ന് സമയം കളയണ്ട ഞൻ CK ഗ്രൂപ്പിൽ മാത്രം വർക്ക് ആവൊള്ളൂ 😝
 
 For More Details Click Help Button Below..
-@CrazyBotsz
+@CinemaaKottaka
 </i>"""    
     
     HELP_TEXT = """
@@ -43,16 +43,15 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/Blur_Glass">Kane Antony</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/KaneKenaz/Charlotte">Click Me</a></i>
 """
